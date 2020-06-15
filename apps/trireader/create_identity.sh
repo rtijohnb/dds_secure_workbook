f9bed5f8cbd432363ec3b5f26fdf03d3d7c8ccf5
user <user@ubuntu18.04.2>
2020-06-15 17:10:48 -0400

    all crypto assets regenerated for demo purposes

--- apps/trireader/create_identity.sh
+++ apps/trireader/create_identity.sh
@@ -6,16 +6,19 @@ app_name=trireader
 echo "Creating identity for ${app_name}:"
 
 echo "+ Private key deployed to './deploy/private/${app_name}.key'"
-openssl genrsa -out ./deploy/private/${app_name}key.pem 2048
+openssl ecparam -name secp256k1 -genkey -noout -out deploy/private/${app_name}key.pem
 
 echo "+ Create Certificate Signing Request: './${app_name}.csr'"
-openssl req -config ${app_name}.cnf -new -key ./deploy/private/${app_name}key.pem -out ${app_name}.csr
+openssl req -new -sha256 -key deploy/private/${app_name}key.pem -out ${app_name}.csr -config ${app_name}.cnf
 
 echo "+ Requesting signing of certificate"
-openssl ca -config ../../ca/identity/openssl.cnf -days 365 -in ${app_name}.csr -out ./deploy/${app_name}.pem
+openssl ca -config ../../ca/identity/openssl.cnf -days 3650 -in ${app_name}.csr -out deploy/${app_name}.pem
 echo "+ Signed certificate deployed to: './deploy/${app_name}.pem'"
 
-# For convenience, copy the template permissions file to one for this app
+echo "+ Removing the .csr file that is no longer needed"
+rm ${app_name}.csr
+
+# For convenience, rename the template permissions file to one for this app
 echo "+ Edit permissions for this app in: '${app_name}_permissions.xml'"
 echo "+ Then sign permissions using sign_permissions.sh"
 mv appname_permissions.xml ${app_name}_permissions.xml
