95585ab85ee26730bac50ecbfca0ceee85807fb0
user <user@ubuntu18.04.2>
2023-05-03 15:48:49 -0400

    updates for 7.0 secp384r1 and sha384

--- ca/permissions/resource/create_permissions_ca.sh
+++ ca/permissions/resource/create_permissions_ca.sh
@@ -23,13 +23,13 @@ echo ""
 echo "Creating a new Permissions CA for signing Permissions and Governance files:"
 echo ""
 echo "  Generating private key: ./private/permissionscakey.pem"
-openssl ecparam -name secp256k1 -genkey -noout -out private/permissionscakey.pem
+openssl ecparam -name secp384r1 -genkey -noout -out private/permissionscakey.pem
 
 echo "  Generating Certificate Signing Request: ca.csr"
-openssl req -new -sha256 -key private/permissionscakey.pem -out ca.csr -config openssl.cnf
+openssl req -new -sha384 -key private/permissionscakey.pem -out ca.csr -config openssl.cnf
 
 echo "  Generating and self-signing the Permissions CA certificate"
-openssl x509 -req -sha256 -days 3650 -in ca.csr -text -signkey private/permissionscakey.pem -out permissionscacert.pem
+openssl x509 -req -sha384 -days 3650 -in ca.csr -text -signkey private/permissionscakey.pem -out permissionscacert.pem
 echo ""
 
 echo "  Deploying Permissions CA's public certificate to ${DDS_SECURE_DEMO_HOME}/app/deployall"
